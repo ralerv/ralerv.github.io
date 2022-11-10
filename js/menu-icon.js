67de81e07e6d1f34dca8e826a548d89a5f711a32
ralerv <ramos_rrv@hotmail.com>
2022-11-10 11:12:25 -0500

    improve menu icon js

--- js/menu-icon.js
+++ js/menu-icon.js
@@ -1,7 +1,7 @@
 let menuIcon = document.getElementById ("menu-icon")
 let menuList = document.getElementById ("nav-list")
 
-menuIcon.addEventListener ("click", showmenu)
+menuIcon.addEventListener("click", showmenu)
 
 function showmenu() {
     menuList.classList.toggle("nav-list--show")
