bc261e46623778f2e5e8105e578a1c166a5a13fa
ralerv <ramos_rrv@hotmail.com>
2022-11-13 14:53:03 -0500

    add nav menu improvement

--- js/menu-icon.js
+++ js/menu-icon.js
@@ -1,9 +1,18 @@
-let menuIcon = document.getElementById ("menu-icon")
-let menuList = document.getElementById ("nav-list")
+let menuIcon = document.getElementById("menu-icon")
+let menuList = document.getElementById("nav-list")
+let menuLink = document.getElementsByClassName("nav-link")
 
 menuIcon.addEventListener("click", showmenu)
+
+for (let index = 0; index < menuLink.length; index++) {//Como usar addEventListener para getElementsbyClass
+    menuLink[index].addEventListener("click", hidemenu) 
+}
 
 function showmenu() {
     menuList.classList.toggle("nav-list--show")
     menuIcon.classList.toggle("menu-icon-img--show")
+}
+
+function hidemenu() {
+    menuList.classList.toggle("nav-list--show")
 }
