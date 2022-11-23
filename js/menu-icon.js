94fcc620107088fec283d25a06accb8cf737b0b2
ralerv <ramos_rrv@hotmail.com>
2022-11-23 16:14:24 -0500

    improve and add coments to js

--- js/menu-icon.js
+++ js/menu-icon.js
@@ -3,20 +3,43 @@ let menuList = document.getElementById("nav-list")
 let menuLink = document.getElementsByClassName("nav-link")
 let divHider = document.getElementById("nav-body-hider")
 
-menuIcon.addEventListener("click", showmenu)
-
-for (let index = 0; index < menuLink.length; index++) {//Como usar addEventListener para getElementsbyClass
-    menuLink[index].addEventListener("click", hidemenu) 
+function crearListeners( ){                 //Crear listeners para los elementos a escuchar
+    menuIcon.addEventListener("click", showmenu);
+    for (let index = 0; index < menuLink.length; index++) {//Como usar addEventListener para getElementsbyClass
+        menuLink[index].addEventListener("click", hidemenu) 
+    }   
 }
 
-function showmenu() {
+function showmenu() {                                   //Pone clases en CSS que muestran el menu
     menuList.classList.toggle("nav-list--show")
     menuIcon.classList.toggle("menu-icon-img--show")
     divHider.classList.toggle("nav-body-hider--show")
 }
 
-function hidemenu() {
+function hidemenu() {                                   //QUITA clases en CSS que muestran el menu
     menuList.classList.toggle("nav-list--show")
     menuIcon.classList.toggle("menu-icon-img--show")
     divHider.classList.toggle("nav-body-hider--show")
 }
+
+function matchFMediaQuerie (width) {
+    if (width.matches) {
+        console.log("haylisteners xd")
+        crearListeners()
+    }
+    else {
+        if (divHider.classList.contains("nav-body-hider--show")){ //Esconder el menu si se quedo abierto en modo vertical al pasar a horizontal
+            hidemenu()
+        }
+        console.log("SE SUPONE QUE YA NO HAY LISTENERS xd")
+        menuIcon.removeEventListener("click", showmenu) //Quitar listener existente a un elemento traido por ID
+        for (let index = 0; index < menuLink.length; index++) { //Quitar listener a elementos de una clase
+            menuLink[index].removeEventListener("click", hidemenu)
+        }
+    }
+}
+    
+let maxWidth1 = window.matchMedia("(max-width: 767px)") // Como crear un media querie en js
+matchFMediaQuerie(maxWidth1)                            // Importante para inicializar la función
+maxWidth1.addEventListener("change",matchFMediaQuerie)
+
